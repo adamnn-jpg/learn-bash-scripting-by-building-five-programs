407479d19968cff5c9aa8599a196cb8fd3e096ab
adamnn-jpg damnn <ramdaniadam761@gmail.com>
2024-10-28 07:21:09 +0000

    Save progress: 817.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-# Program that counts down to zero from a given argument
-
-echo -e "\n~~ Countdown Timer ~~\n"
-
-: '
-for (( i = $1; i >= 0; i-- ))
-do
-  echo $i
-  sleep 1
-done
-'
-I=$1
-
-echo $I
+#Bingo Number Generator
+echo -e "\n~~ Bingo Number Generator ~~\n"
+NUMBER=5
+echo $NUMBER
